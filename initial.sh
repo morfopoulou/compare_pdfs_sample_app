5a7471a33b743104a7761c81d24db709c944ee6e
morfopoulou <144283109+morfopoulou@users.noreply.github.com>
2024-09-26 15:45:39 +0200

    Update initial.sh

--- initial.sh
+++ initial.sh
@@ -3,7 +3,7 @@ export GCP_PROJECT='arctic-analyzer-435209-m1'  # Change this
 export GCP_REGION='europe-west3'
 export GCP_BUCKET='demo_gradio_data'
 export AR_REPO='cloud-run-source-deploy'
-export SERVICE_NAME='sample-app-skoda'
+export SERVICE_NAME='compare-pdfs'
 #either copy paste to Cloud Shell or run
 #chmod +x initial.sh
 #source ./initial.sh
